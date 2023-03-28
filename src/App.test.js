a2a47ec806bff988a2e9fae5fe78f29beb92aedf
vanessakallen <va.vanessaallen@gmail.com>
2023-03-28 11:09:49 -0700

    update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('Renders h1 text', () => {
   render(<App />);
   const textContent = screen.getByText("Blogging App");
   expect(textContent).toBeInTheDocument();
